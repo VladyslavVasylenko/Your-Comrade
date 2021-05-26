764b51fc8c9827a1a3c25f5eb515af6a3de95ab9
unknown <vladyslavvasylenko03@gmail.com>
2021-05-26 16:24:03 +0300

    save

--- your-comrade/src/Main/Block-Search/FetchUser.jsx
+++ your-comrade/src/Main/Block-Search/FetchUser.jsx
@@ -2,7 +2,6 @@ import React, { Component } from "react";
 import './index.css';
 
 const BASE_PATH = "https://api.worldoftanks.ru/wot/account/list/?application_id=aa1e24b44bb69aac50de3419c0c1ac07";
-const SEARCH_PATH = '';
 const SEARCH_PARAM = '&search=';
 
 class FetchUser extends Component {
@@ -18,25 +17,23 @@ class FetchUser extends Component {
   }
 
   fetchData = () => {
-    fetch(`${BASE_PATH}${SEARCH_PARAM}${SEARCH_PATH}`)
+    fetch(`${BASE_PATH}${SEARCH_PARAM}`)
     .then(res => res.json())
     .then((result) => {this.setState(result)})
     .catch(error => error);
   }
 
   handleChange(event) {
-    console.log(event);
     this.setState({value: event.target.value});
   }
 
   handleSubmit(event) {
-    console.log(event);
     event.preventDefault();
   }
 
   componentDidMount() {
-    const {search} = this.state;
-    this.fetchData(search);
+    const {value} = this.state;
+    this.fetchData(value);
   }
 
   render() {
@@ -44,22 +41,22 @@ class FetchUser extends Component {
     return (
       <div>
         <form className="form-search" onSubmit={this.handleSubmit}>
-            <input 
-            className="input-field" 
-            name="name"
-            placeholder="Nickname"
-            type="text" 
-            value={this.state.value} 
-            onChange={this.handleChange} 
-            />
-            <input 
-            type="submit" 
-            className="btn" 
-            value="Search"/>
+          <input 
+          className="input-field" 
+          name="name"
+          placeholder="Nickname"
+          type="text" 
+          value={this.state.value} 
+          onChange={this.handleChange} 
+          />
+          <input 
+          type="submit" 
+          className="btn" 
+          value="Search"/>
         </form>
         <div className="result"></div>
       </div>
-    )
+    );
   }
 }
 
