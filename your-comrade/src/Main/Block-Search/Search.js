f3c3cdc29be8afbd2d5548d2550e0686bd101a77
unknown <vladyslavvasylenko03@gmail.com>
2021-06-01 12:15:05 +0300

    fixes status

--- your-comrade/src/Main/Block-Search/Search.js
+++ your-comrade/src/Main/Block-Search/Search.js
@@ -2,41 +2,44 @@ import React, { Component } from "react";
 // import FetchUser from './FetchUser';
 import Form from './Form';
 import ResultSearch from './ResultSearch';
+import './index.css';
 
 const API_KEY = "aa1e24b44bb69aac50de3419c0c1ac07";
 const SEARCH_PARAM = '&search=';
 
-class Search extends Component() {
+class Search extends Component {
   constructor(props) { 
     super(props);
     this.state = {
+      // status: undefined,
       nickname: undefined,
       account_id: undefined,
-      error: undefined,
+      error: "",
     }
   }
 
   gettingResultSearch = async (e) => {
     e.preventDefault();
     let nickname = e.target.elements.nickname.value;
+    let status;
 
     if(nickname) {
       const api_url = await
       fetch(`https://api.worldoftanks.ru/wot/account/list/?application_id=${API_KEY}${SEARCH_PARAM}`);
-      const status = await api_url.json();
+      status = await api_url.json();
       console.log(status);
     }
 
-    this.setState({
-      nickname: status.data.nickname,
-      account_id: status.data.account_id,
-      error: "",
-    });
+      this.setState({
+        nickname: status.data.nickname,
+        account_id: status.data.account_id,
+        error: "",
+      });
   }
 
   render() {
     return (
-      <div className="search__conteiner">
+      <div className="search__conteiner" id="Search">
         {/* <FetchUser /> */}
         <Form ResultSearch={this.gettingResultSearch} />
         <ResultSearch 
