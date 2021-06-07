5322dafd9268428de8df4f1b98423537b93c336d
VasylenkoVladyslav <vladyslavvasylenko03@gmail.com>
2021-06-07 20:54:58 +0300

    fixes

--- your-comrade/src/Main/Block-Contact/Contact.js
+++ your-comrade/src/Main/Block-Contact/Contact.js
@@ -4,7 +4,7 @@ function Contact() {
   return(
     <div className="contact" id="Contact">
       <div className="conteiner-contact">
-        <p className="contact__mail"><a className="contact__mail" href="https://www.google.com/intl/uk/gmail/about/">vladyslavvasylenko03@gmail.com</a></p>
+        <p className="contact__mail"><a className="contact__mail" href="https://www.google.com/intl/uk/gmail/about/" target="blank">vladyslavvasylenko03@gmail.com</a></p>
         <p className="contact__phone"><a className="contact__phone" href="tel:++380668047228">+380668047228</a></p>
         <p className="contact__inst"><a href="https://www.instagram.com/vladyslavvasylenko0312/" target="blank"><i className="fab fa-instagram"></i></a></p>
         <p className="contact__In"><a href="https://www.linkedin.com/in/vladyslav-vasylenko-5600301a6/" target="blank"><i className="fab fa-linkedin-in"></i></a></p>
